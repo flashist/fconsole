89539c45e2b29e5c33c054b208501d91afa488fd
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-08-29 17:09:58 +0300

    In progress.

--- npm-link.sh
+++ npm-link.sh
@@ -5,8 +5,13 @@ if [ -z "$1" ]
     exit 1
 fi
 
-curPackageName=$(eval 'cut -d "=" -f 2 <<< $(npm run env | grep "npm_package_name")')
-echo "Current package name $curPackageName"
+npm run build
+
+CURRENT_FOLDER=$(pwd);
+echo "Current folder patch $CURRENT_FOLDER"
+
+CURRENT_PACKAGE_NAME=$(eval 'cut -d "=" -f 2 <<< $(npm run env | grep "npm_package_name")')
+echo "Current package name $CURRENT_PACKAGE_NAME"
 
 echo "Adding the current folder is added as a global npm link"
 cd dist/
@@ -14,8 +19,18 @@ npm link
 
 # Go back to the root of package
 cd "../";
-# Go to the target folder
-cd "${1}"
 
-echo "Link packakge $curPackageName in the target folder"
-npm link "$curPackageName"
+# Build of all passed projects
+while [ "$1" ]; do
+    echo $1
+
+    # Go to the target folder
+    cd "${1}"
+
+    npm link "$CURRENT_PACKAGE_NAME"
+    echo "Link packakge $CURRENT_PACKAGE_NAME in the target folder"
+
+    cd ${CURRENT_FOLDER}
+
+    shift
+done
